32d3b6d7bab1cbf81902629e391add127cbb98a1
christoffsen <christopher.elder8@gmail.com>
2020-11-10 22:40:12 -0800

    Adding nearly complete solution for assign3c

--- 7031assign3c/app/src/main/java/school/comp7031/assignment3/FramedImageView.java
+++ 7031assign3c/app/src/main/java/school/comp7031/assignment3/FramedImageView.java
@@ -1,26 +1,19 @@
 package school.comp7031.assignment3;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
-import android.os.Build;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.ImageView;
 
 public class FramedImageView extends View {
-    private int borderColor = Color.RED;
+    private int borderColor = Color.BLUE;
     private int borderWidth = 5;
-    private Drawable mExampleDrawable;
     private Bitmap image;
     private Bitmap scaledImage;
 
@@ -33,6 +26,12 @@ public class FramedImageView extends View {
 
     public FramedImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FramedImageView, 0, 0);
+        
+        borderColor = Color.parseColor(a.getString(R.styleable.FramedImageView_borderColor));
+        borderWidth = Integer.parseInt(a.getString(R.styleable.FramedImageView_borderWidth));
+
         init(attrs, 0);
     }
 
@@ -42,68 +41,45 @@ public class FramedImageView extends View {
     }
 
     private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.FramedImageView, defStyle, 0);
-        this.setMinimumHeight(android.R.attr.height);
-        this.setMinimumWidth(android.R.attr.width);
         image = BitmapFactory.decodeResource(getResources(), R.drawable.dog);
-        a.recycle();
-        invalidateTextPaintAndMeasurements();
+        setScaleAndInvalidate();
     }
 
-    private void invalidateTextPaintAndMeasurements() {
-        scaledImage = Bitmap.createScaledBitmap(image, this.getWidth() - (2 * borderWidth), this.getHeight() - (2 * borderWidth), false);
+    private void setScaleAndInvalidate() {
+        int width = (getRight() - getLeft()) - (2 * borderWidth);
+        int height = (getBottom() - getTop()) - (2 * borderWidth);
+        if(width < 0) {
+            width = image.getWidth() - (2 * borderWidth);
+        }
+        if(height < 0) {
+            height = image.getHeight() - (2 * borderWidth);
+        }
+
+        scaledImage = Bitmap.createScaledBitmap(image, width, height, false);
+        this.invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
 
         Paint backgroundPaint = new Paint();
 
         backgroundPaint.setColor(borderColor);
-        canvas.drawRect(getLeft(), getTop(), getRight(), getBottom(), backgroundPaint);
+        canvas.drawRect(0, 0, getWidth(), getHeight(), backgroundPaint);
 
-        canvas.drawBitmap(scaledImage, getLeft() + borderWidth, getTop() - borderWidth, new Paint());
+        canvas.drawBitmap(scaledImage, borderWidth, borderWidth, null);
 
     }
 
     public void setImage(Bitmap newImage){
         image = newImage;
-        invalidateTextPaintAndMeasurements();
+        setScaleAndInvalidate();
     }
 
     public void setImage(Integer integer) {
         image = BitmapFactory.decodeResource(getResources(), integer);
-        invalidateTextPaintAndMeasurements();
+        setScaleAndInvalidate();
     }
 
-    public void setBorderColor(int newColor){
-        this.borderColor = newColor;
-    }
-
-    public int getBorderColor(){
-        return this.borderColor;
-    }
-
-    public void setBorderWidth(int newWidth){
-        this.borderWidth = newWidth;
-    }
-
-    public int getBorderWidth(){
-        return this.borderWidth;
-    }
-
-    @Override
-    public void onMeasure(int width, int height){
-        this.setMeasuredDimension(scaledImage.getWidth() + borderWidth * 2, scaledImage.getHeight() + borderWidth * 2);
-    }
 }
